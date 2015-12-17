6a83bd496fb6b53e5afa53197f1839c8ab83d5ba
Mikhail Itkin <itkin.m@gmail.com>
2015-12-17 14:53:40 +0100

    first blog build

---
+++ blog/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["2015/12/17/hello_world","master","pages/about"],objects:{},objnames:{},objtypes:{},terms:{about:1},titles:["Hello World!","Sitemap","About"],titleterms:{about:2,hello:0,sitemap:1,world:0}})
